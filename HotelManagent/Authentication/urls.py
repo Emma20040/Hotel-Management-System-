ded6d0fb5d75525f17f6669b1dc31a7a7829a747
Emma <emmanuelfongong10@gmail.com>
2026-01-15 21:56:53 +0100

    Sign-up done using customUserCreation form

--- HotelManagent/Authentication/urls.py
+++ HotelManagent/Authentication/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
+from Authentication import views
 urlpatterns = [
+    path('home/', views.home, name ='home'),
+    path("signup/", views.signup, name="signup"),
     
 ]
--- HotelManagent/Authentication/views.py
+++ HotelManagent/Authentication/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse
+from Authentication.forms import CustomUserCreationForm
 
-# Create your views here.
+# Home 
+def home(request):
+    return render(request, 'Authentication/home.html')
+
+
+# Signup Service
+def signup(request):
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('home'))
+    else:
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, 'Authentication/signup.html', context)
