b589430f8f8b3063f85c536bf7eba9a3628f8da2
Emma <emmanuelfongong10@gmail.com>
2026-02-13 14:40:58 +0100

    added  booking and payment models and also added more fields to the guest model

--- HotelManagent/Authentication/models.py
+++ HotelManagent/Authentication/models.py
@@ -44,4 +44,27 @@ class Room(models.Model):
 
 class Guest(models.Model):
     guest_id = models.AutoField(primary_key=True)
-    first_name = models
+    first_name = models.CharField(max_length=200, default="nan")
+    laast_name = models.CharField(max_length=200, default="nan")
+    date_of_birth = models.DateField(default="")
+    address = models.CharField(max_length=200, default="")
+    phone = models.CharField(max_length=20, blank=True, default="added after")
+    email = models.EmailField(unique=True, blank=False, default="late@gmail.com")
+
+
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    room_number = models.ForeignKey(Room, on_delete=models.CASCADE)
+    guest_id = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+    price = models.BigIntegerField()
+
+
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    Booking_id = models.ForeignKey(Booking, on_delete=models.CASCADE)
+    amont = models.BigIntegerField()
+    payment_method = models.CharField()
+    payment_date = models.DateTimeField(auto_now=True)
+
